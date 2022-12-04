2a72a8e24fb52cd136d4b3a99c4c124b8f0cd063
Filip Dornak <fdornak@gmail.com>
2022-12-04 09:44:57 +0100

    Day 4

---
+++ AoC22/Day4.fs
@@ -0,0 +1,51 @@
+﻿module AoC22.Day4
+
+open System.IO
+
+type Sectors = { First: int * int; Second: int * int }
+
+let fileSplitByLines: string [] =
+    (File.ReadAllText "day4.txt").Split("\r\n")
+    |> Array.filter (fun str -> not (Seq.isEmpty str))
+
+let parseIntoSectors (str: string) : Sectors =
+    let split = str.Split(',')
+
+    let splitByDashAndMap (s: string) = s.Split('-') |> Array.map int
+
+    let first = splitByDashAndMap split[0]
+    let second = splitByDashAndMap split[1]
+
+    let fRange = (first[0], first[1])
+    let sRange = (second[0], second[1])
+
+    { First = fRange; Second = sRange }
+
+let isOneFullyContained (sectors: Sectors) : bool =
+    match sectors with
+    | { First = (fa, fb); Second = (sa, sb) } when (sa >= fa && sb <= fb) || (fa >= sa && fb <= sb) -> true
+    | _ -> false
+
+let isAnyOverlap (sectors: Sectors) : bool =
+    match sectors with
+    //fully contained
+    | sectors when isOneFullyContained sectors -> true
+    //left overlap
+    | { First = (fa, _); Second = (sa, sb) } when (fa >= sa && fa <= sb) -> true
+    //right overlap
+    | { First = (_, fb); Second = (sa, sb) } when (fb >= sa && fb <= sb) -> true
+    | _ -> false
+
+let firstStar () : int =
+    fileSplitByLines
+    |> Array.map parseIntoSectors
+    |> Array.map isOneFullyContained
+    |> Array.filter (fun x -> x = true)
+    |> Array.length
+
+let secondStar () : int =
+    fileSplitByLines
+    |> Array.map parseIntoSectors
+    |> Array.map isAnyOverlap
+    |> Array.filter (fun x -> x = true)
+    |> Array.length
---
+++ AoC22Test/Day4Test.fs
@@ -0,0 +1,9 @@
+﻿module AoC22.Day4Test
+
+open Xunit
+open FsUnit.Xunit
+
+[<Fact>]
+let ``Day 4`` () =
+    Day4.firstStar () |> should equal 602
+    Day4.secondStar () |> should equal 891
