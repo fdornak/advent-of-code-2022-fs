a6f3593dbfdc5941ff0d0fb2317b9e931f8d1ea0
Filip Dornak <fdornak@gmail.com>
2022-12-06 11:31:25 +0100

    Day 6

---
+++ AoC22/Day6.fs
@@ -0,0 +1,23 @@
+﻿module AoC22.Day6
+
+open System.IO
+
+let private input: string =
+    (File.ReadAllText "day6.txt")
+
+let private hasUniqueCharacters (str: char []) : bool =
+    let uniqueLength =
+        str |> Array.distinct |> Array.length
+
+    uniqueLength <> str.Length
+
+let private findNonUniqueMarkerIndex (windowLength: int) (str: string) : int =
+    str
+    |> Seq.windowed windowLength
+    |> Seq.takeWhile hasUniqueCharacters
+    |> Seq.length
+    |> (fun x -> x + windowLength)
+
+let firstStar () : int = input |> findNonUniqueMarkerIndex 4
+
+let secondStar () : int = input |> findNonUniqueMarkerIndex 14
---
+++ AoC22Test/Day6Test.fs
@@ -0,0 +1,9 @@
+﻿module AoC22.Day6Test
+
+open Xunit
+open FsUnit.Xunit
+
+[<Fact>]
+let ``Day 6`` () =
+    Day6.firstStar () |> should equal 1142
+    Day6.secondStar () |> should equal 2803
